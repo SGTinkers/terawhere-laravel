9aa95c790df8cdcc732e1d50ee31918ad76544d0
xmodulus <azizsafudin@yahoo.com.sg>
2017-05-03 22:54:08 +0800

    chg migration files

--- app/Booking.php
+++ app/Booking.php
@@ -10,7 +10,7 @@ class Booking extends Model
 {
 	use SoftDeletes;
 
-    protected $fillable = ['user_id' ,'offer_id', 'status'];
+    protected $fillable = ['user_id' ,'offer_id', 'status', 'driver_remarks','rating'];
 	
 	protected $dates = ['deleted_at'];
 
