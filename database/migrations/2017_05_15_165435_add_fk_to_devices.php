2060658c924265353526ceb8887e97c09631ad11
xmodulus <azizsafudin@yahoo.com.sg>
2017-05-16 00:56:11 +0800

    Added devices migration

---
+++ database/migrations/2017_05_15_165435_add_fk_to_devices.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFkToDevices extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::table('devices', function (Blueprint $table) {
+        $table->foreign('user_id')->references('id')->on('users');          
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('devices', function (Blueprint $table) {
+        $table->dropForeign('devices_user_id_foreign');
+
+        });
+    }
+}
