f658f3af7a321ec2adbebb5d7ccaaea4c7abf85a
Faruq Rasid <me@ruqqq.sg>
2017-05-18 22:01:08 +0800

    Run fmt.

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -5,55 +5,52 @@ namespace Tests\Feature;
 use App\User;
 use JWTAuth;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class AuthTest extends TestCase
 {
-    /**
-     * Test Facebook Login
-     * Exchanges facebook token to our servers JWT Token.
-     *
-     * @return void
-     */
-    public function testAuthFacebook()
-    {
-      if (!env('FB_TOKEN')) {
-        $this->assertTrue(true);
-        return;
-      }
-
-      $response = $this->json('POST', '/api/v1/auth', ['token' => env('FB_TOKEN'), 'service' => 'facebook']);
-
-      $response
-        ->assertStatus(200)
-        ->assertJson([
-          'token' => true,
-          'user' => true,
-        ]);
+  /**
+   * Test Facebook Login
+   * Exchanges facebook token to our servers JWT Token.
+   *
+   * @return void
+   */
+  public function testAuthFacebook()
+  {
+    if (!env('FB_TOKEN')) {
+      $this->assertTrue(true);
+      return;
     }
+
+    $response = $this->json('POST', '/api/v1/auth', ['token' => env('FB_TOKEN'), 'service' => 'facebook']);
+
+    $response
+      ->assertStatus(200)
+      ->assertJson([
+        'token' => true,
+        'user'  => true,
+      ]);
+  }
 
   /**
    * Test /api/v1/auth/refresh
    *
    * @return void
    */
-    public function testRefresh()
-    {
-      $user = User::first();
-      $token = JWTAuth::fromUser($user);
+  public function testRefresh()
+  {
+    $user  = User::first();
+    $token = JWTAuth::fromUser($user);
 
-      $response = $this->json('GET', '/api/v1/auth/refresh', [], ['Authorization' => 'Bearer ' . $token]);
+    $response = $this->json('GET', '/api/v1/auth/refresh', [], ['Authorization' => 'Bearer ' . $token]);
 
-      $response
-        ->assertStatus(200)
-        ->assertHeader('Authorization')
-        ->assertExactJson([
-          'status' => 'Ok',
-          'message' => 'Check Authorization Header for new token',
-        ]);
-    }
+    $response
+      ->assertStatus(200)
+      ->assertHeader('Authorization')
+      ->assertExactJson([
+        'status'  => 'Ok',
+        'message' => 'Check Authorization Header for new token',
+      ]);
+  }
 
   /**
    * Test /api/v1/me
@@ -62,7 +59,7 @@ class AuthTest extends TestCase
    */
   public function testGetAuthenticatedUser()
   {
-    $user = User::first();
+    $user  = User::first();
     $token = JWTAuth::fromUser($user);
 
     $response = $this->json('GET', '/api/v1/me', [], ['Authorization' => 'Bearer ' . $token]);
