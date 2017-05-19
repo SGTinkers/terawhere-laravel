b58a69bfbcad82e2775cbcae54f2b16ce22b44c3
xmodulus <azizsafudin@yahoo.com.sg>
2017-05-20 00:54:22 +0800

    Fix for authcontroller

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -144,9 +144,9 @@ class AuthenticateController extends Controller
   public function getAuthenticatedUser()
   {
     $user = Auth::user();
-    $user->put('roles', $user->roles);
+    $roles = $user->roles;
 
     // the token is valid and we have found the user via the sub claim
-    return response()->json(compact('user'));
+    return response()->json(compact('user','roles'));
   }
 }
