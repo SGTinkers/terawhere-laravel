02e2b4bcc00ed497299e3e203f9b698c2c430d21
Faruq Rasid <me@ruqqq.sg>
2017-05-19 11:02:25 +0800

    Add constants for Offer statuses.

--- app/Offer.php
+++ app/Offer.php
@@ -9,6 +9,14 @@ class Offer extends Model
 {
   use SoftDeletes;
 
+  const STATUSES = [
+    'CANCELLED'           => 0,
+    'PENDING'             => 1,
+    'ONGOING'             => 2,
+    'COMPLETED'           => 3,
+    'EXPIRED'             => 4,
+  ];
+
   protected $fillable = ['user_id', 'meetup_time', 'start_name', 'start_addr', 'start_lat', 'start_lng', 'end_name', 'end_addr', 'end_lat', 'end_lng', 'vacancy', 'remarks', 'status', 'pref_gender', 'vehicle_number', 'vehicle_desc', 'vehicle_model', 'start_geohash', 'end_geohash',
   ];
   protected $dates = ['deleted_at'];
