2e4e7805b4006195d7bebb94ceba0944d6732986
xmodulus <azizsafudin@yahoo.com.sg>
2017-05-20 20:52:44 +0800

    return offer object with show booking and users bookings

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -52,7 +52,7 @@ class BookingController extends Controller
         'message' => 'Booking does not exist.',
       ], 404);
     }
-
+    $booking['offer']  = $booking->offer;
     $booking['name']   = $booking->user->name;
     $booking['gender'] = $booking->user->gender;
 
@@ -203,7 +203,9 @@ class BookingController extends Controller
   public function getUsersBookings(GetUserId $request)
   {
     $bookings = Booking::withTrashed()->where('user_id', Auth::user()->id)->get();
-
+    foreach($bookings as $booking){
+        $booking['offer'] = $booking->offer;
+    }
     return response()->json([
       'data' => $bookings,
     ], 200);
