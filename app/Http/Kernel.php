88f7f12c381be5a75d5489ef74d0bf219606c764
xmodulus <azizsafudin@yahoo.com.sg>
2017-05-27 07:57:15 +0800

    Added admin middleware to kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,5 +59,6 @@ class Kernel extends HttpKernel
     'throttle'    => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     'jwt.auth'    => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
     'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
+    'admin'       => \App\Http\Middleware\VerifyAdminAccess::class,
   ];
 }
